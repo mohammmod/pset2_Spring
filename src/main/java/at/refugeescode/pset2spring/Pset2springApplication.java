00cd13cdd2d98b4dc252cb5b9c1f1d5775c8701e
mohammmod <mohammad.sawas2@outlook.com>
2018-03-23 12:50:27 +0100

    initialize the project

---
+++ src/main/java/at/refugeescode/pset2spring/Pset2springApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.pset2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pset2springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pset2springApplication.class, args);
+	}
+}
